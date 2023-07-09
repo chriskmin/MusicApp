e030f95998df08e4216ca358e778f27891e077eb
Chris Min <chrismin@Bu.edu>
2023-07-09 00:12:18 -0400

    Creating sidebar at the moment

---
+++ components/Sidebar.tsx
@@ -0,0 +1,44 @@
+"use client";
+import {usePathname} from "next/navigation";
+import { useMemo } from "react";
+import {HiHome} from "react-icons/hi"
+import {BiSearch} from "react-icons/bi"
+
+interface SidebarProps{
+    children: React.ReactNode;
+}
+
+ 
+const Sidebar: React.FC<SidebarProps>= ({
+    children 
+}) => {
+    const pathname = usePathname();
+
+    const routes = useMemo(() => [
+    {
+        icon: HiHome,
+        label: 'Home',
+        active: pathname !== '/search',
+        href: '/'
+    },
+    {
+        icon: BiSearch,
+        label: 'Search',
+        active: pathname == '/search',
+        href: '/search',
+
+    }
+], [pathname]);
+
+
+  return (
+    <div className="flex h-full">
+        <div>
+
+            
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar
