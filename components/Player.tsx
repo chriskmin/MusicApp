4161176d0ac8e2d26737310ad29877d995ad8ea8
Chris Min <chrismin@Bu.edu>
2023-07-22 21:30:50 -0400

    Creating music playability function

--- components/Player.tsx
+++ components/Player.tsx
@@ -1,10 +1,28 @@
+"use client"
+import usePlayer from "@/hooks/usePlayer"
+import useURLSongLoad from "@/hooks/useURLSongLoad";
+import usesongid from "@/hooks/usesongid";
+
 
 const Player = () => {
-  return (
-    <div>
-        Player
-    </div>
-  )
+    const player = usePlayer();
+    const {song} = usesongid(player.activeId)
+    const songUrl = useURLSongLoad(song!);
+
+    
+    if (!song || !songUrl || !player.activeId) {
+        return null;
+    }
+
+
+    return(
+
+        <div>
+
+
+        </div>
+    )
+
 }
 
 export default Player
