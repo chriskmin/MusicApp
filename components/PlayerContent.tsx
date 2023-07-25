5cba41049c1871d3b0386e5c31ff6a9b0eb3997d
Chris Min <chrismin@Bu.edu>
2023-07-24 23:35:16 -0400

    Currently creating player

--- components/PlayerContent.tsx
+++ components/PlayerContent.tsx
@@ -1,6 +1,12 @@
 "use client"
 
 import { Song } from "@/types";
+import MediaItem from "./MediaItem";
+import LikeButton from "./LikeButton";
+import {BsPauseFill, BsPlayFill} from "react-icons/bs"
+import { AiFillBackward } from "react-icons/ai";
+
+
 
 interface PlayerContentProps {
     song: Song;
@@ -8,10 +14,61 @@ interface PlayerContentProps {
 }
   
 
-const PlayerContent = () => {
-  return (
-    <div>
-      
+const PlayerContent: React.FC<PlayerContentProps> = ({ 
+    song, 
+    songUrl
+}) => {
+
+  const Icon = true ? BsPauseFill: BsPlayFill;
+  
+  return(
+
+    <div className="grid grid-cols-2 md:grid-cols-3 h-full">
+        <div className="flex w-full justify-start">
+            <div className="flex items-center gap-x-4">
+                <MediaItem data={song} />
+                <LikeButton songId={song.id}/>
+            </div>
+        </div>
+        <div className="
+            flex 
+            md:hidden 
+            col-auto 
+            w-full 
+            justify-end 
+            items-center
+        ">
+            <div 
+            onClick={() => {}}
+            className="
+              h-10
+              w-10
+              flex 
+              items-center 
+              justify-center 
+              rounded-full 
+              bg-white 
+              p-1 
+              cursor-pointer
+            ">
+                <Icon size={30} className="text-black"/>
+
+            </div>
+        </div>
+        <div className="
+            hidden
+            h-full
+            md:flex 
+            justify-center 
+            items-center 
+            w-full 
+            max-w-[722px] 
+            gap-x-6
+        ">
+            <AiFillBackward /> 
+
+
+        </div>
     </div>
   )
 }
