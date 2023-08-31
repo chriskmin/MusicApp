45140806547ea5d5ab7394e6d95b9c5ea11c0150
Chris Min <chrismin@Bu.edu>
2023-08-30 22:36:12 -0400

    updates

--- hooks/useBeatDrop.py
+++ hooks/useBeatDrop.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/beat_drop', methods=['POST'])
 def beat_drop():
     audio_file = request.files['file']
-    beat_drop_time = find_beat_drop(audio_file)  # Assuming find_beat_drop() is the function you implemented earlier
+    beat_drop_time = find_beat_drop(audio_file)  
     return jsonify({'beat_drop_time': beat_drop_time})
 
 if __name__ == '__main__':
